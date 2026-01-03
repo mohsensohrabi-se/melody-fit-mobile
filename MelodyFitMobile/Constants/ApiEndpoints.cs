7ad8795aba76d77208a7e40cf22a51126c826fb2
mohsensohrabi-se <mohsen.sohrabi.se@gmail.com>
2026-01-03 19:25:10 +0330

    feat : Add a class to include api endpoints and add Register as the first method

---
+++ MelodyFitMobile/Constants/ApiEndpoints.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MelodyFitMobile.Constants
+{
+    public static class ApiEndpoints
+    {
+        public const string Register = "api/auth/register";
+    }
+}
